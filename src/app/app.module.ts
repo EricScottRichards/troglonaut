f30dd7921eeb630353c2484de48e4fda1a2ef8cb
EricRichards <erichards@jive.com>
2017-10-24 15:27:57 -0600

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+
+import { environment } from '../environments/environment'
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabaseModule } from 'angularfire2/database'
+import { AngularFireAuthModule } from 'angularfire2/auth'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'letslearn-dev'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyB4hLQttGZpE-0WhIRtRTHrIP7zsKLmPiU",
+    authDomain: "troglonaut.firebaseapp.com",
+    databaseURL: "https://troglonaut.firebaseio.com",
+    projectId: "troglonaut",
+    storageBucket: "troglonaut.appspot.com",
+    messagingSenderId: "427161961494"
+  },
   production: false
 };
