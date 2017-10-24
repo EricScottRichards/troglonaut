39b469572df1edd69890ca64583eacbd9cad15d2
EricRichards <erichards@jive.com>
2017-10-24 15:36:39 -0600

    shared

---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth'
+import * as firebase from 'firebase/app'
+import { Observable } from 'rxjs/Observable'
+
+// visit https://medium.com/letsboot/lets-learn-how-to-install-and-setup-angularfire2-4-0-135d72bb0a41 
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+}
